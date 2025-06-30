e8faf3bfed219b434e995a3399775ea48c78c160
Abdul-Awul-Arman <abdulawularman52114@gmail.com>
2025-06-30 18:19:37 +0600

    fixed modal auto showing

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -14,7 +14,7 @@ type User = {
 
 export default function User({ name, email, isRegistered, fetchAndSetUsers }: User) {
   const [isShow, setShow] = useState(false);
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   const { updateUser, error } = useUpdateUsers();
   const { deleteUser } = useDeleteUsers();
 
